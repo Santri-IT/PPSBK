06316a0686d847b4c522553e228717a791ab07a5
Santri-IT <teknooga@gmail.com>
2024-10-07 17:28:57 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- bootstrap --}}
+    <!-- Tambahkan Bootstrap CSS dari CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer"  />
+    <title>PPSBK</title>
+</head>
+<body>
+    {{-- navbar --}}
+    <nav class="navbar navbar-expand navbar-light bg-light py-2 shadow">
+        <div class="container">
+            <a href="" class="navbar-brand d-flex align-items-center">
+                <img src="" alt="">
+                <span>PPSBK</span>
+            </a>
+
+            <div class="justify-content-between">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Profile</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    {{-- navbar end --}}
+
+    {{-- header --}}
+    {{-- header end --}}
+
+    {{-- content --}}
+    {{-- content end --}}
+
+    {{-- footer --}}
+    {{-- footer end --}}
+
+    <!-- Tambahkan Bootstrap JavaScript dan Popper dari CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Tambahkan Icon Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</body>
+</html>
