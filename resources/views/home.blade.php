b2f4d32e4798bc44f9d028ec7206c5ae7dd732cc
Santri-IT <teknooga@gmail.com>
2024-10-07 23:13:23 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,23 +11,45 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer"  />
+    
+    {{-- panggil css --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>PPSBK</title>
 </head>
+
 <body>
     {{-- navbar --}}
-    <nav class="navbar navbar-expand navbar-light bg-light py-2 shadow">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light py-2 shadow">
         <div class="container">
-            <a href="" class="navbar-brand d-flex align-items-center">
-                <img src="" alt="">
-                <span>PPSBK</span>
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <img src="{{ asset('images/navbar.png') }}" alt="" style="height: 50px" class="me-2">
+                <span style="font-size: 70%">PONPES <br>SYEKH BURHANUDDIN</span><hr>
             </a>
+            {{-- tampilan android --}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="justify-content-between">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item"><a href="" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">Profile</a></li>
-                    <li class="nav-item"><a href="" class="nav-link">About</a></li>
+            <div class="collapse navbar-collapse justify-content-between" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a href="/home" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+                    <li class="nav-item"><a href="#services" class="nav-link"><i class="fas fa-user"></i> Profil</a></li>
+                    <li class="nav-item"><a href="/news" class="nav-link"><i class="fas fa-book"></i> Sejarah</a>
+                    </li>
+                    <div class="dropdown">
+                        <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-file-alt"></i> Pendaftaran</a>
+                      
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                          <li><a class="dropdown-item" href="#">Wustho</a></li>
+                          <li><a class="dropdown-item" href="#">Ulya</a></li>
+                        </ul>
+                      </div>
                 </ul>
+                <br>
+                <div>
+                    <div id="clock" class="clock bg-dark text-light px-2 rounded d-none d-sm-block"></div>
+                </div>
             </div>
         </div>
     </nav>
@@ -46,4 +69,5 @@
     <!-- Tambahkan Icon Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </body>
+
 </html>
